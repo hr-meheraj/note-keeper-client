33dedfd2afed5be617fef446658893ffd2139446
hr-meheraj <hrmeheraj2021@gmail.com>
2022-07-31 23:47:12 +0600

    Bug fixed, event bubbling

--- src/components/ViewNote.jsx
+++ src/components/ViewNote.jsx
@@ -22,8 +22,8 @@ function ViewNote({ viewNoteData, setViewNoteData, refetch }) {
     return (
         <div>
             <input type="checkbox" id="viewNote" class="modal-toggle" />
-            <label for='viewNote' class="modal dark:bg-[#1e1e26ed]  modal-bottom sm:modal-middle">
-                <label for='' class="modal-box bg-[#F0F0F1] text-gray-900 dark:bg-[#1b2129] dark:text-[#a5a2a2] md:text-[18px] text-[12px]  text-[#abaaaa] font-extralight md:leading-[38px] dark:text-white">
+            <label for='viewNote' onClick={() => setViewNoteData("")} class="modal dark:bg-[#1e1e26ed]  modal-bottom sm:modal-middle">
+                <label for='' onClick={e => e.stopPropagation()} class="modal-box bg-[#F0F0F1] text-gray-900 dark:bg-[#1b2129] dark:text-[#a5a2a2] md:text-[18px] text-[12px]  text-[#abaaaa] font-extralight md:leading-[38px] dark:text-white">
                     <h3 class="font-bold text-lg">{viewNoteData.title || "Please try Again"}</h3>
                     <p class="py-4 text-black dark:text-[#ffffff69]">{viewNoteData.details || "If you want to see this post please click again on view button, firstly It may be a problem to see note"}</p>
                     {
